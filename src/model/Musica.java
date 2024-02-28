f420138ba3925a494941642e98deede4f830ca55
bernardo <baavieira@sga.pucminas.br>
2024-02-28 17:15:12 -0300

    adicionando base de dados binario

---
+++ src/model/Musica.java
@@ -0,0 +1,121 @@
+
+import java.io.IOException;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+public class Musica{
+    private int id;
+    private int rank;
+    private int streams;
+    private String nome;
+    private Date data;
+    private String regiao;
+
+    private String titulo;
+
+    public Musica(int rank, int streams, String nome, String data, String regiao, String titulo) throws IOException {
+        this.rank = rank;
+        this.streams = streams;
+        this.nome = nome;
+        this.regiao = regiao;
+        this.titulo = titulo;
+        MusicaService temp = new MusicaService("DB/teste.db");
+        temp.raf.seek(0);
+        this.id = temp.raf.readInt() + 1;
+        temp.raf.writeInt(this.id);
+        // aqui eu estou formatando a estrutura da data, como eu quero que ela apareca. EX: 2017-12-31
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            //  transformando a string recebida como parametro em uma do tipo Date
+            this.data = format.parse(data);
+        }
+        catch(ParseException e){
+            //capturando a excecao, se houver
+            e.printStackTrace();
+        }
+        temp = null;
+    }
+    // getters e setters
+
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public int getRank() {
+        return rank;
+    }
+
+    public int getStreams() {
+        return streams;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public String getRegiao() {
+        return regiao;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    public void setStreams(int streams) {
+        this.streams = streams;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDataByString(String data) throws ParseException {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        this.data = format.parse(data);
+        format = null;
+    }
+    public void setData(Date data){
+        this.data = data;
+    }
+
+    public void setRegiao(String regiao) {
+        this.regiao = regiao;
+    }
+
+    public Musica() throws IOException {
+
+        MusicaService temp = new MusicaService("DB/teste.db");
+
+        temp.raf.seek(0);
+        if(temp.file.length() > 0)
+            this.id = temp.raf.readInt() + 1;
+        else
+            this.id = 0;
+
+        temp.raf.seek(0);
+        temp.raf.writeInt(this.id);
+        this.nome ="";
+        this.regiao = "";
+        this.streams = 0;
+        data = new Date();
+    }
+
+}
--- src/service/MusicaService.java
+++ src/service/MusicaService.java
@@ -1,2 +1,169 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.lang.instrument.ClassDefinition;
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.UnmodifiableClassException;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.security.KeyStore;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
+import java.lang.instrument.Instrumentation;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.jar.JarFile;
+
 public class MusicaService {
-}
+
+    protected final File file;
+
+    protected final RandomAccessFile raf;
+
+
+    public MusicaService(String file) throws IOException {
+        this.file = new File(file);
+        if(!this.file.exists()){
+            this.file.createNewFile();
+        }
+        this.raf = new RandomAccessFile(this.file,"rw");
+
+    }
+    protected void writeInFile(long pos, Musica musica) throws IOException {
+        raf.seek(pos);
+        raf.writeBoolean(true);
+        raf.writeInt(getObjectSize(musica));
+        raf.writeInt(musica.getId());
+        raf.writeUTF(musica.getTitulo());
+        raf.writeUTF(musica.getNome());
+        raf.writeLong(musica.getData().getTime());
+        raf.writeInt(musica.getRank());
+        raf.writeInt(musica.getStreams());
+        // Escrever os dados de texto (nome e região) como strings, limitando a regiao a somente 3 caracteres(String fixa)
+        String temp = musica.getRegiao().substring(0,Math.min(3, musica.getRegiao().length()));
+        byte [] substring = temp.getBytes();
+        raf.write(substring);
+    }
+
+   protected int getObjectSize(Musica musica){
+        return 4+4+4+8+musica.getNome().getBytes().length+musica.getTitulo().getBytes().length+3;
+
+    }
+
+    protected Musica readFromFile(long pos) throws IOException {
+
+        raf.seek(pos);
+        Musica temp = new Musica();
+        raf.readByte();
+        raf.readInt();
+        temp.setId(raf.readInt());
+        temp.setTitulo(readString(raf.getFilePointer()));
+        temp.setNome(readString(raf.getFilePointer()));
+        temp.setData(Date.from(Instant.ofEpochMilli(raf.readLong())));
+        temp.setRank(raf.readInt());
+        temp.setStreams(raf.readInt());
+        temp.setRegiao(byteArrayToString(raf.getFilePointer()));
+
+
+        return temp;
+    }
+    protected void readAll() throws IOException {
+        long pos = 4;
+        for(int i = 0; i < 20; i++){
+           System.out.println(toString(readFromFile(pos)));
+            pos = raf.getFilePointer();
+        }
+
+
+    }
+
+
+
+    protected Musica readFromCSV(long pos) throws IOException, ParseException {
+        raf.seek(pos);
+        String temp = raf.readLine();
+        Musica resp = new Musica();
+        String [] data = temp.split(",", 4);
+        resp.setTitulo(data[0]);
+        resp.setRank(Integer.parseInt(data[1]));
+        resp.setDataByString(data[2]);
+        String base = data[3];
+
+        if(base.charAt(0) == '\"'){
+             base = base.substring(1);
+
+             data = base.split("\"", 2);
+
+             resp.setNome(data[0]);
+
+             data = data[1].split(",", 3);
+             resp.setRegiao(data[1]);
+             resp.setStreams(Integer.parseInt(data[2]));
+        }
+        else {
+            data = base.split(",", 3);
+            resp.setNome(data[0]);
+            resp.setRegiao(data[1]);
+            resp.setStreams(Integer.parseInt(data[2]));
+        }
+
+        return resp;
+
+    }
+
+    protected String byteArrayToString(long pos) throws IOException {
+        raf.seek(pos);
+        byte [] temp = new byte[3];
+        for(int i = 0; i < temp.length; i++){
+            temp[i] = raf.readByte();
+        }
+        String resp = new String(temp, StandardCharsets.UTF_8);
+        return resp;
+
+
+    }
+    protected String readString(long pos) throws IOException {
+        raf.seek(pos);
+        short temp = raf.readShort();
+        byte [] newString = new byte[temp];
+        for(int i = 0; i < temp; i++){
+            newString[i] = raf.readByte();
+        }
+        return new String(newString, StandardCharsets.UTF_8);
+
+    }
+
+
+
+
+   public String toString(Musica musica){
+        return "\nID: " + musica.getId() +
+                "\nNome: "+musica.getNome()+
+                "\nTitulo:"+musica.getTitulo()+
+                "\nRegiao: "+musica.getRegiao() +
+                "\nData: "+ musica.getData() +
+                "\nRank: "+musica.getRank() +
+                "\nStreams: "+musica.getStreams();
+
+    }
+
+
+
+
+
+
+
+     }
+
+
+
+
+
+
+
