3ae9517b3c66332280df19573070bf281d9d862e
bernardo <baavieira@sga.pucminas.br>
2024-02-28 19:45:29 -0300

    create/read

--- src/app/main.java
+++ src/app/main.java
@@ -10,22 +10,25 @@ public class main {
 
     public static void main(String[] args) throws IOException, ParseException {
 
-        MusicaService serviceDB = new MusicaService("db/teste.db");
+        MusicaService serviceDB = new MusicaService("DB/teste.db");
 
         MusicaService serviceCSV = new MusicaService("DB/csv_teste.csv");
         serviceCSV.raf.readLine();
         long posCSV = serviceCSV.raf.getFilePointer();
         long posBD = 4;
-        for(int i = 0; i < 20; i++){
+      /* for(int i = 0; i < 20; i++){
             serviceDB.writeInFile(posBD,serviceCSV.readFromCSV(posCSV)) ;
             posCSV = serviceCSV.raf.getFilePointer();
             posBD = serviceDB.raf.getFilePointer();
         }
         serviceDB.raf.seek(0);
         System.out.println("ULTIMO ID ---->");
-        System.out.println(serviceDB.raf.readInt());
+        System.out.println(serviceDB.raf.readInt());*/
 
-        serviceDB.readAll();
+        //serviceDB.readAll();
+
+        System.out.println(serviceDB.toString(serviceDB.Read(4, 4)));
+        //serviceDB.createMusica("DB/teste.db");
 
 
 
