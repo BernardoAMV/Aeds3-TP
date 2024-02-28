1104134da38d3cd9cc6071f952f3fa8583918ae2
Bernardo <baavieira@sga.pucminas.br>
2024-02-28 17:08:27 -0300

    att

---
+++ src/app/main.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+public class main {
+       protected MusicaService service;
+       protected Musica musica;
+
+
+    public static void main(String[] args) throws IOException, ParseException {
+
+        MusicaService serviceDB = new MusicaService("DB/teste.db");
+
+        MusicaService serviceCSV = new MusicaService("DB/csv_teste.csv");
+        serviceCSV.raf.readLine();
+        long posCSV = serviceCSV.raf.getFilePointer();
+        long posBD = 4;
+        for(int i = 0; i < 20; i++){
+            serviceDB.writeInFile(posBD,serviceCSV.readFromCSV(posCSV)) ;
+            posCSV = serviceCSV.raf.getFilePointer();
+            posBD = serviceDB.raf.getFilePointer();
+        }
+        serviceDB.raf.seek(0);
+        System.out.println(serviceDB.raf.readInt());
+
+        serviceDB.readAll();
+
+
+
+
+
+
+    }
+}
